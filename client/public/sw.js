44b34ab598ab11221e6261ad0d51257791413f0f
Bill Zhou <billtt@gmail.com>
2025-07-16 16:32:53 +0800

    add sw.js

---
+++ client/public/sw.js
@@ -0,0 +1,55 @@
+// PWA Service Worker for TimeMachine
+const CACHE_NAME = 'timemachine-v3';
+const urlsToCache = [
+  '/',
+  './manifest.json',
+  './logo.png',
+  './favicon.ico'
+];
+
+// Install event - cache essential resources
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Install event');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Service Worker: Caching files');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activate event');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        // Return offline page if available
+        if (event.request.mode === 'navigate') {
+          return caches.match('/');
+        }
+      })
+  );
+});
